3a9402bac1dc445bc130d71d5bd600658afd1e50
skhodjaev <skhodjaev@icloud.com>
2024-05-28 11:37:11 +0500

    initial commit

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,26 @@
+console.log(100);
+
+console.log('Hello World!');
+
+console.log(20, 'Hello', true);
+
+const x = 100;
+
+console.log(x);
+
+console.error('error');
+
+console.warn('warning');
+
+console.table({ name: 'skhodjaev', email: 'skhodjaev@gmail.com' });
+
+console.group('simple');
+console.log(x);
+console.error('error');
+console.warn('warning');
+console.groupEnd();
+
+const styles =
+  'padding: 20px; background-color: white; color: red; font-weight: bold';
+
+console.log('%cHello World', styles);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Modern JavaScript from the beginning 2.0 course by Brad Traversy notes and exercises
+
+![Course Image](https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/themes/2152872931/settings_images/3073a32-ffb3-e3d7-c106-171cca61401c_13ec59a1-5fa0-4999-9de1-ed2063a8ad7c.jpg)
+
+All the notes I take during the course and the `javascript` exercises will be in the appropriate folders.
