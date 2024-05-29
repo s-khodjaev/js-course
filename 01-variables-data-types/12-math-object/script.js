f0bbb61a4627a9dfe0c139d142004450f03c0ba0
skhodjaev <skhodjaev@icloud.com>
2024-05-29 23:26:10 +0500

    added numbers, math and numbers challenge

---
+++ 01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,34 @@
+// let x;
+
+// x = Math.sqrt(9);
+
+// x = Math.abs(-5);
+
+// x = Math.round(4.8);
+
+// x = Math.ceil(4.3);
+
+// x = Math.floor(4.9);
+
+// x = Math.pow(2, 3);
+
+// x = Math.min(4, 5, 3);
+
+// x = Math.max(5, 6, 3);
+
+// x = Math.random();
+
+// x = Math.floor(Math.random() * 10 + 1);
+
+// console.log(x);
+
+let x;
+x = Math.floor(Math.random() * 100 + 1);
+
+let y;
+y = Math.floor(Math.random() * 50 + 1);
+
+let sum = x + y;
+let sumOutput = `${x} + ${y} = ${sum}`;
+
+console.log(sumOutput);
