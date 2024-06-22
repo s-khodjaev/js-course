a2a3f589ac294a97c38bf07c4a5e85c19e526d23
Shukhrat Khodjaev <skhodjaev@Shukhrats-MacBook-Air.local>
2024-06-22 23:08:44 +0500

    added challenge

--- 06-DOM/03-create-elements/shopping-list/script.js
+++ 06-DOM/03-create-elements/shopping-list/script.js
@@ -53,3 +53,23 @@ function createListItemClean(item) {
 }
 
 createListItemClean('Banana');
+
+// Way 3
+
+function createNewItem(item) {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
+
+  const button = document.createElement('button');
+  button.className = 'remove-item btn-link text-red';
+
+  const icon = document.createElement('i');
+  icon.className = 'fa-solid fa-xmark';
+  button.appendChild(icon);
+
+  li.appendChild(button);
+
+  document.querySelector('.items').appendChild(li);
+}
+
+createNewItem('Mangoes');
